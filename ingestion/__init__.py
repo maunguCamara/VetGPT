d89bf4cfe00063a3ac73fbd61be17019ed849e3d
Aramrac <camaramaungu@gmail.com>
2026-04-07 12:29:01 +0100

    Updated init.py

--- ingestion/__init__.py
+++ ingestion/__init__.py
@@ -1,12 +1,17 @@
-from .pdf_parser import VetPDFParser, ParsedDocument, ParsedPage
-from .chunker import VetChunker, DocumentChunk
-from .embedder import VetVectorStore
+from .wikivet_scraper import WikiVetScraper, ScrapedArticle
+from .pubmed_scraper import PubMedScraper, PubMedArticle
+from .fao_scraper import FAOScraper, FAODocument
+from .eclinpath_scraper import EClinPathScraper, EClinPathArticle
+from .pipeline import ScrapingPipeline
 
 __all__ = [
-    "VetPDFParser",
-    "ParsedDocument",
-    "ParsedPage",
-    "VetChunker",
-    "DocumentChunk",
-    "VetVectorStore",
+    "WikiVetScraper",
+    "ScrapedArticle",
+    "PubMedScraper",
+    "PubMedArticle",
+    "FAOScraper",
+    "FAODocument",
+    "EClinPathScraper",
+    "EClinPathArticle",
+    "ScrapingPipeline",
 ]
